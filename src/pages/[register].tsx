eb6208370ead8eaa5a9030d2199c0d4b35bfe2a2
Achette <i.achette@live.com>
2023-01-29 12:48:40 -0300

    Feat: created Register page

--- src/pages/[register].tsx
+++ src/pages/[register].tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { Text, Stack, Box, Flex, Heading, Button } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Stack, Text } from "@chakra-ui/react";
 import Head from "next/head";
+import Link from "next/link";
 import { BackgroundImage } from "../components";
 
-export default function Home() {
+export default function Register() {
   return (
     <Box display="flex">
       <Head>
@@ -20,17 +20,21 @@ export default function Home() {
           Welcome,
         </Heading>
         <Text marginBottom="10rem" textAlign="center">
-          Please, register ou sign-in to continue
+          Please, register to access Weekly Planner
         </Text>
         <Box>
-          <Stack direction="column" spacing={4} align="center">
-            <Button colorScheme="purple" w="370px" borderRadius="3.125rem">
-              Register
-            </Button>
+          <Stack direction="row" spacing={4} align="center">
+            <Link href={`/register`}>
+              <Button colorScheme="purple" w="180px" borderRadius="3.125rem">
+                Register
+              </Button>
+            </Link>
 
-            <Button colorScheme="purple"  w="370px" borderRadius="3.125rem">
-              Log-In
-            </Button>
+            <Link href={`/`}>
+              <Button colorScheme="facebook" w="180px" borderRadius="3.125rem">
+                Back
+              </Button>
+            </Link>
           </Stack>
         </Box>
       </Flex>
---
+++ src/pages/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Text, Stack, Box, Flex, Heading, Button } from "@chakra-ui/react";
+import Head from "next/head";
+import { BackgroundImage } from "../components";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <Box display="flex">
+      <Head>
+        <title>Weekly Planner</title>
+      </Head>
+      <Flex
+        flex={1}
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+        h="100vh"
+      >
+        <Heading fontWeight="100" fontSize="4rem" textAlign="center" top={0}>
+          Welcome,
+        </Heading>
+        <Text marginBottom="10rem" textAlign="center">
+          Please, register ou sign-in to continue
+        </Text>
+        <Box>
+          <Stack direction="column" spacing={4} align="center">
+            <Link href={`/register`}>
+              <Button colorScheme="purple" w="370px" borderRadius="3.125rem">
+                Register
+              </Button>
+            </Link>
+
+            <Button colorScheme="purple" w="370px" borderRadius="3.125rem">
+              Log-In
+            </Button>
+          </Stack>
+        </Box>
+      </Flex>
+      <Box flex={1} h="100vh">
+        <BackgroundImage />
+      </Box>
+    </Box>
+  );
+}
